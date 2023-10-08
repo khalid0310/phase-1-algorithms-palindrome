b6aa596af08ffc5a7380f550331f2595de06d84f
khalid <khalid.mohamed@moringaschool.com>
2023-10-08 15:03:08 +0300

    finished assignment

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(str) {
+
+  const charArray = str.split('');
+
+  const reversedStr = charArray.reverse().join('');
+  
+  return str === reversedStr;
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
-module.exports = isPalindrome;
+console.log(isPalindrome("madam"));   // true
+console.log(isPalindrome("robot"));   // false
+console.log(isPalindrome("racecar")); // true
+console.log(isPalindrome("hello"));   // false
